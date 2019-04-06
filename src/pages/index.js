e2b3a579bdfbccc2509957b8016184a45f023cf1
Chris Barnes <chris@Chriss-Mac-Pro.local>
2019-04-06 10:28:15 -0400

    Add dragon and unicorn image to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,9 +11,33 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hey Friends. Welcome to Dragons vs. Unicorns.</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <StaticQuery
+        query={graphql`
+          query {
+            unicornImage: file(relativePath: { eq: "unicorn.png" }) {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            },
+            dragonImage: file(relativePath: { eq: "dragon.png" }) {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        `}
+        render={data => (
+          <div>
+            <Img fluid={data.unicornImage.childImageSharp.fluid} />
+            <Img fluid={data.dragonImage.childImageSharp.fluid} />
+          </div>
+        )}
+      />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
