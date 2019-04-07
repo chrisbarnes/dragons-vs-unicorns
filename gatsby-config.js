2b9df8071739f00c92b68646178632cfc29e6731
Chris Barnes <chris@Chriss-Mac-Pro.local>
2019-04-07 14:10:26 -0400

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,18 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-689067-18",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
